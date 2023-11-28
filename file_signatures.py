a7baf2c0103894514c45c0b88f235d635f88fa37
JavaFraud <135260449+JavaFraud@users.noreply.github.com>
2023-11-28 19:08:37 +0100

    Now returns only one result

--- file_signatures.py
+++ file_signatures.py
@@ -30,18 +30,18 @@ def read_file_hex(file_path):
         hex_data = binascii.hexlify(binary_data).decode('utf-8')
         return hex_data
 
+def get_file_header_info(file_hex, file_sigs: dict):
+    for file_sig in file_sigs:
+        if file_hex[0:len(file_sig["Header (hex)"].lower().replace(' ',''))] == file_sig["Header (hex)"].lower().replace(' ',''):
+            result = get_file_sig_info(file_sig)
+    return result
 #-----------------------------------------------------------
 
-file_to_analyse_path = os.getcwd()+'\\'+'BurningKermit.jpg'
+file_to_analyse_path = os.getcwd()+'\\'+'testFile.png'
 file_sigs_json_path = os.getcwd()+'\\'+'file_sigs.json'
 
-file_to_alayse_hex_format = read_file_hex(file_to_analyse_path)
+file_to_analyse_hex = read_file_hex(file_to_analyse_path)
+file_sigs = get_file_sigs_from_json(file_sigs_json_path)
 
-some_data = get_file_sigs_from_json(file_sigs_json_path)
-
-for file_sig in some_data:
-    if file_to_alayse_hex_format[0:len(file_sig["Header (hex)"].lower().replace(' ',''))] == file_sig["Header (hex)"].lower().replace(' ',''):
-        print(get_file_sig_info(file_sig))
-
-# Longest header
-# 3C 3F 78 6D 6C 20 76 65 72 73 69 6F 6E 3D 22 31 2E 30 22 3F 3E 0D 0A 3C 4D 4D 43 5F 43 6F 6E 73 6F 6C 65 46 69 6C 65 20 43 6F 6E 73 6F 6C 65 56 65 72 73 69 6F 6E 3D 22
+result = get_file_header_info(file_to_analyse_hex, file_sigs)
+print(result)
