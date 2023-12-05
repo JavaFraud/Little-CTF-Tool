8647f78ed6ad665654be715db0dc1b350fdb556c
JavaFraud <135260449+JavaFraud@users.noreply.github.com>
2023-12-05 22:45:53 +0100

    messing around with binary and hex

--- My Little CTF tool/steg.py
+++ My Little CTF tool/steg.py
@@ -14,17 +14,15 @@ for key, bit in enumerate(binarydata):
 
 #convert new binary to hex
 hex_steg_message = hex(int(binary_steg_message[1:],2))
-print(hex_steg_message)
+
+hidden_message = bytes.fromhex(str(hex_steg_message[2:])).decode('latin-1')
+
+print(hidden_message)
 #exclude header :)
 
 #add element at the beginning of the binary file so the lists starts at 1 and we'r'e able to divide by 8 to ge the LSB
 
-'''
 #separate list elements
-for key, element in enumerate(data):
-
-print(bin(int(data[0:10],16)))
-'''
 
 
 #get last bit
