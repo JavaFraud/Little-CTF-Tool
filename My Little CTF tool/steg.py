a59da8da8ca5db1e2d53184a9699989a64fc4710
JavaFraud <135260449+JavaFraud@users.noreply.github.com>
2023-12-06 11:36:09 +0100

    keeping going on the steg. It's quite interresting but not usefull at all for future tools rn. Still I'm learning so it's usefull in the end actually. COMMIT

--- My Little CTF tool/steg.py
+++ My Little CTF tool/steg.py
@@ -7,7 +7,7 @@ binarydata = bin(int(data,16))
 binarydata = '2'+binarydata[2:]
 binary_steg_message = ''
 
-#get 8th bit and build a string from it
+#get LSB bit and build a string from it
 for key, bit in enumerate(binarydata):
     if key%8 == 0:
         binary_steg_message += bit
@@ -15,16 +15,41 @@ for key, bit in enumerate(binarydata):
 #convert new binary to hex
 hex_steg_message = hex(int(binary_steg_message[1:],2))
 
-hidden_message = bytes.fromhex(str(hex_steg_message[2:])).decode('latin-1')
 
-print(hidden_message)
+spaced_hex_message = ""
+
+
+
+for key, byte in enumerate(hex_steg_message):
+    if key %2 != 0:
+        spaced_hex_message += byte +" "
+    else:
+        spaced_hex_message += byte
+spaced_hex_message = spaced_hex_message[2:]
+listed_hex_message = spaced_hex_message.split(" ")
+
+hidden_message = ""
+for key, byte in enumerate(listed_hex_message):
+    try:
+        decoded_byte = bytes.fromhex(str(byte)).decode('utf-8')
+        hidden_message += decoded_byte + " "
+    except UnicodeDecodeError:
+        print("No UTF-8 decoding found on the " + byte + " byte")
+list_hidden_message = hidden_message.split(" ")
+print(list_hidden_message)
+
+
+'''
+final_message = list_hidden_message
+
+for char in list_hidden_message:
+    if " " not in char:
+        final_message.remove(char)
+'''
 #exclude header :)
 
 #add element at the beginning of the binary file so the lists starts at 1 and we'r'e able to divide by 8 to ge the LSB
 
 #separate list elements
-
-
-#get last bit
 
 #regroup the bits 8 by 8 to form new hew --> convert to whatever
